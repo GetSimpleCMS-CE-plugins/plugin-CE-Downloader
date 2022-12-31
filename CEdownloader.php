f58c2ad39ff8c4dee9271b894779093ac82e9550
GetSimpleCMS-CE <119761508+GetSimpleCMS-CE@users.noreply.github.com>
2022-12-31 14:00:49 +0000

    Add files via upload

---
+++ CEdownloader.php
@@ -0,0 +1,28 @@
+<?php
+ 
+
+# get correct id for plugin
+$thisfile=basename(__FILE__, ".php");
+
+# register plugin
+register_plugin(
+	$thisfile, //Plugin id
+	'CE Downloader', 	//Plugin name
+	'1.0', 		//Plugin version
+	'Multicolor',  //Plugin author
+	'', //author website
+	'This plugin is for download plugins supports php 8', //Plugin description
+	'plugins', //page type - on which admin tab to display
+	'downloader'  //main function (administration)
+);
+
+ 
+# add a link in the admin tab 'theme'
+add_action('plugins-sidebar','createSideMenu',array($thisfile,'CE Downloader'));
+
+ 
+
+function downloader() {
+include(GSPLUGINPATH.'CEdownloader/downloader.php'); 
+};
+?>
