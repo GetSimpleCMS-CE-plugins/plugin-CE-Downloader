378380861a42476411e6d0a7e9b31ec901fae7b8
risingisland <risingisland@yahoo.com>
2022-12-31 14:43:21 +0000

    Update CEdownloader.php

--- CEdownloader.php
+++ CEdownloader.php
@@ -7,11 +7,11 @@ $thisfile=basename(__FILE__, ".php");
 # register plugin
 register_plugin(
 	$thisfile, //Plugin id
-	'CE Downloader', 	//Plugin name
+	'CE Plugin Downloader', 	//Plugin name
 	'1.0', 		//Plugin version
 	'Multicolor',  //Plugin author
 	'', //author website
-	'This plugin is for download plugins supports php 8', //Plugin description
+	'This plugin is to download plugins that supports php8.x', //Plugin description
 	'plugins', //page type - on which admin tab to display
 	'downloader'  //main function (administration)
 );
